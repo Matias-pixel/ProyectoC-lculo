ff8f25fab0819990cafaf529e39269b1b15b5566
Matias-pixel <marchantmatias706@gmail.com>
2024-03-31 17:25:37 -0300

    Segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,48 @@
-import React from "react";
-import { Button, Carousel  } from "flowbite-react";
-export const App = () =>{
+import React, { useState } from "react";
+import { create, all } from "mathjs";
 
-  return(
+export const App = () => {
+  const math = create(all);
 
-    <>
-    <div className="grid grid-cols-12">
-      
-      <div className="col-span-10 container mt-16 ">
+  const [solution, setSolution] = useState("");
 
-          <h1>hola</h1>
+  const [inequalities, setInequalities] = useState([
+    "x1 + 2 * x2 <= 5",
+    "2 * x1 - 3 * x2 > 1",
+  ]);
 
+  const handleInequalityChange = (e) => {
+    const newInequalities = [...inequalities];
+    newInequalities[0] = e.target.value; // Actualizamos solo la primera inecuación por ahora
+    setInequalities(newInequalities);
+  };
+
+  const solveSystem = () => {
+    try {
+      // Convertir las inecuaciones a un formato compatible con Math.js
+      const equations = inequalities.map(inequality => inequality.replace(/x(\d+)/g, 'x[$1]'));
+      // Resolver el sistema de inecuaciones
+      const solutions = math.lusolve(equations);
+      // Mostrar las soluciones
+      console.log('Soluciones:', solutions);
+      setSolution(solutions);
+    } catch (error) {
+      console.error('Error al resolver el sistema de inecuaciones:', error);
+    }
+  };
+
+  return (
+    <div className="container mx-auto mt-10">
+      <div className="flex gap-5 ">
+        <h2>Ingrese las inecuaciones lineales</h2>
+        <input
+          type="text"
+          value={inequalities.join(", ")} // Unimos las inecuaciones con una coma y un espacio
+          onChange={handleInequalityChange}
+        />
+        <button onClick={solveSystem}>Resolver</button>
+        <div>{solution && <p>Solución: {solution}</p>}</div>
       </div>
     </div>
-    </>
-  )
-}
+  );
+};
